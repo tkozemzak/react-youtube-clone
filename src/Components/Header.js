3b9c8b7e56cada78f75791e934c3ef2e3787d5e9
tkozemzak <timkozemzak@gmail.com>
2021-03-02 11:26:29 -0700

    add header and header icons

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import '../Header.css'
+import MenuIcon from '@material-ui/icons/Menu'
+import SearchIcon from '@material-ui/icons/Search'
+import VideoCallIcon from '@material-ui/icons/VideoCall';
+import AppsIcon from '@material-ui/icons/Apps';
+import NotificationsIcon from '@material-ui/icons/Notifications'
+import { Avatar } from '@material-ui/core';
+
 
 const Header = () => {
     return (
         <div className="header">
-            
+            <MenuIcon/>
+            <img className="header_logo" src="https://i.pinimg.com/originals/31/23/9a/31239a2f70e4f8e4e3263fafb00ace1c.png"alt=""/>
+            <input type="text"/>
+            <SearchIcon/>
+            <VideoCallIcon/>
+            <AppsIcon/>
+            <NotificationsIcon/>
+            <Avatar
+                alt="Tim"
+                src="https://avatars.githubusercontent.com/u/37643202?s=460&u=fe6687e49e5b3cbfebd985ab52c2167744ec1a85&v=4"
+                />
         </div>
     )
 }
