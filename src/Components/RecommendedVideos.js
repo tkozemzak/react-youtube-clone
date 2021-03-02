f7d6506d4479e07635baa6896b62cf14faff9241
tkozemzak <timkozemzak@gmail.com>
2021-03-02 13:56:52 -0700

    create card for each video

--- src/Components/RecommendedVideos.js
+++ src/Components/RecommendedVideos.js
@@ -1,10 +1,134 @@
 import React from 'react'
 import '../Styles/RecommendedVideos.css'
+import VideoCard from './VideoCard'
 
 const RecommendedVideos = () => {
     return (
         <div className="recommended-videos">
             <h2>Recommended</h2>
+            <div className="recommended-videos_videos">
+                <VideoCard 
+                    title="Video Title"
+                    views="2.3M Views"
+                    timestamp="3 days ago"
+                    channelImage="https://avatars.githubusercontent.com/u/37643202?s=460&u=fe6687e49e5b3cbfebd985ab52c2167744ec1a85&v=4"
+                    channel="Channel Name"
+                    image="https://i.ytimg.com/vi/w6fPVCdcVHk/maxresdefault.jpg"
+                />
+                <VideoCard 
+                    title="Video Title"
+                    views="2.3M Views"
+                    timestamp="3 days ago"
+                    channelImage="https://avatars.githubusercontent.com/u/37643202?s=460&u=fe6687e49e5b3cbfebd985ab52c2167744ec1a85&v=4"
+                    channel="Channel Name"
+                    image="https://i.ytimg.com/vi/w6fPVCdcVHk/maxresdefault.jpg"
+                />
+                <VideoCard 
+                    title="Video Title"
+                    views="2.3M Views"
+                    timestamp="3 days ago"
+                    channelImage="https://avatars.githubusercontent.com/u/37643202?s=460&u=fe6687e49e5b3cbfebd985ab52c2167744ec1a85&v=4"
+                    channel="Channel Name"
+                    image="https://i.ytimg.com/vi/w6fPVCdcVHk/maxresdefault.jpg"
+                />
+                <VideoCard 
+                    title="Video Title"
+                    views="2.3M Views"
+                    timestamp="3 days ago"
+                    channelImage="https://avatars.githubusercontent.com/u/37643202?s=460&u=fe6687e49e5b3cbfebd985ab52c2167744ec1a85&v=4"
+                    channel="Channel Name"
+                    image="https://i.ytimg.com/vi/w6fPVCdcVHk/maxresdefault.jpg"
+                />
+                <VideoCard 
+                    title="Video Title"
+                    views="2.3M Views"
+                    timestamp="3 days ago"
+                    channelImage="https://avatars.githubusercontent.com/u/37643202?s=460&u=fe6687e49e5b3cbfebd985ab52c2167744ec1a85&v=4"
+                    channel="Channel Name"
+                    image="https://i.ytimg.com/vi/w6fPVCdcVHk/maxresdefault.jpg"
+                />
+                <VideoCard 
+                    title="Video Title"
+                    views="2.3M Views"
+                    timestamp="3 days ago"
+                    channelImage="https://avatars.githubusercontent.com/u/37643202?s=460&u=fe6687e49e5b3cbfebd985ab52c2167744ec1a85&v=4"
+                    channel="Channel Name"
+                    image="https://i.ytimg.com/vi/w6fPVCdcVHk/maxresdefault.jpg"
+                />
+                <VideoCard 
+                    title="Video Title"
+                    views="2.3M Views"
+                    timestamp="3 days ago"
+                    channelImage="https://avatars.githubusercontent.com/u/37643202?s=460&u=fe6687e49e5b3cbfebd985ab52c2167744ec1a85&v=4"
+                    channel="Channel Name"
+                    image="https://i.ytimg.com/vi/w6fPVCdcVHk/maxresdefault.jpg"
+                />
+                <VideoCard 
+                    title="Video Title"
+                    views="2.3M Views"
+                    timestamp="3 days ago"
+                    channelImage="https://avatars.githubusercontent.com/u/37643202?s=460&u=fe6687e49e5b3cbfebd985ab52c2167744ec1a85&v=4"
+                    channel="Channel Name"
+                    image="https://i.ytimg.com/vi/w6fPVCdcVHk/maxresdefault.jpg"
+                />
+                <VideoCard 
+                    title="Video Title"
+                    views="2.3M Views"
+                    timestamp="3 days ago"
+                    channelImage="https://avatars.githubusercontent.com/u/37643202?s=460&u=fe6687e49e5b3cbfebd985ab52c2167744ec1a85&v=4"
+                    channel="Channel Name"
+                    image="https://i.ytimg.com/vi/w6fPVCdcVHk/maxresdefault.jpg"
+                />
+                <VideoCard 
+                    title="Video Title"
+                    views="2.3M Views"
+                    timestamp="3 days ago"
+                    channelImage="https://avatars.githubusercontent.com/u/37643202?s=460&u=fe6687e49e5b3cbfebd985ab52c2167744ec1a85&v=4"
+                    channel="Channel Name"
+                    image="https://i.ytimg.com/vi/w6fPVCdcVHk/maxresdefault.jpg"
+                />
+                <VideoCard 
+                    title="Video Title"
+                    views="2.3M Views"
+                    timestamp="3 days ago"
+                    channelImage="https://avatars.githubusercontent.com/u/37643202?s=460&u=fe6687e49e5b3cbfebd985ab52c2167744ec1a85&v=4"
+                    channel="Channel Name"
+                    image="https://i.ytimg.com/vi/w6fPVCdcVHk/maxresdefault.jpg"
+                />
+                <VideoCard 
+                    title="Video Title"
+                    views="2.3M Views"
+                    timestamp="3 days ago"
+                    channelImage="https://avatars.githubusercontent.com/u/37643202?s=460&u=fe6687e49e5b3cbfebd985ab52c2167744ec1a85&v=4"
+                    channel="Channel Name"
+                    image="https://i.ytimg.com/vi/w6fPVCdcVHk/maxresdefault.jpg"
+                />
+                <VideoCard 
+                    title="Video Title"
+                    views="2.3M Views"
+                    timestamp="3 days ago"
+                    channelImage="https://avatars.githubusercontent.com/u/37643202?s=460&u=fe6687e49e5b3cbfebd985ab52c2167744ec1a85&v=4"
+                    channel="Channel Name"
+                    image="https://i.ytimg.com/vi/w6fPVCdcVHk/maxresdefault.jpg"
+                />
+                <VideoCard 
+                    title="Video Title"
+                    views="2.3M Views"
+                    timestamp="3 days ago"
+                    channelImage="https://avatars.githubusercontent.com/u/37643202?s=460&u=fe6687e49e5b3cbfebd985ab52c2167744ec1a85&v=4"
+                    channel="Channel Name"
+                    image="https://i.ytimg.com/vi/w6fPVCdcVHk/maxresdefault.jpg"
+                />
+                <VideoCard 
+                    title="Video Title"
+                    views="2.3M Views"
+                    timestamp="3 days ago"
+                    channelImage="https://avatars.githubusercontent.com/u/37643202?s=460&u=fe6687e49e5b3cbfebd985ab52c2167744ec1a85&v=4"
+                    channel="Channel Name"
+                    image="https://i.ytimg.com/vi/w6fPVCdcVHk/maxresdefault.jpg"
+                />
+                
+            </div>
         </div>
     )
 }
---
+++ src/Components/VideoCard.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Avatar from "@material-ui/core/Avatar"
+import '../Styles/VideoCard.css'
+
+
+const VideoCard = ({ image, title, channel, views, timestamp, channelImage}) => {
+    return (
+        <div className="videoCard">
+            <img className="videoCard_thumbnail" src={image} alt=""/>
+            <div className="videoCard_info">
+                <Avatar className="videoCard_avatar"alt={channel} src={channelImage}/>
+                <div className="video_text">
+                    <h4>{title}</h4>
+                    <p>{channel}</p>
+                    <p>{views} . {timestamp}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default VideoCard
