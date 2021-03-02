e2561cafd5c963449f49e17c082a5c8965179529
tkozemzak <timkozemzak@gmail.com>
2021-03-02 16:08:58 -0700

    format channelrow data, add conditionals for formatting

--- src/Components/ChannelRow.js
+++ src/Components/ChannelRow.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../Styles/ChannelRow.css";
 import { Avatar } from "@material-ui/core";
+import VerifiedIcon from "@material-ui/icons/CheckCircleOutlineOutlined";
 
 const ChannelRow = ({
   image,
@@ -10,14 +11,26 @@ const ChannelRow = ({
   noOfVideos,
   description,
 }) => {
+  console.log("length of description:", description.length);
   return (
     <div className="channelrow">
       <Avatar className="channelrow_logo" alt={channel} src={image} />
       <div className="channelrow_text">
-        <h4>{channel}</h4>
+        <h2>
+          {channel} {verified && <VerifiedIcon />}
+        </h2>
         <p>
-          {subs} {subs == 1 ? "subscriber" : "subscribers"}
+          {subs} {subs !== 1 ? "subscribers" : "subscriber"} • {noOfVideos}{" "}
+          videos
         </p>
+        <p>
+          {description.length > 100
+            ? `${description.slice(0, 100)} ...`
+            : description}
+        </p>
+      </div>
+      <div className="channelrow_subbutton">
+        <button>Subscribe</button>
       </div>
     </div>
   );
--- src/Components/VideoRow.js
+++ src/Components/VideoRow.js
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const VideoRow = () => {
-    return (
-        <div>
-            Video Row
-        </div>
-    )
-}
+  return <div></div>;
+};
 
-export default VideoRow
+export default VideoRow;
